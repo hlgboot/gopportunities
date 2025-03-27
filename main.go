6da6be0e050527f0875b927e0dc66d6243f63f7d
Matheus Henriques <matheushlg10@gmail.com>
2025-03-27 19:22:08 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/hlgboot/gopportunities/config"
 	"github.com/hlgboot/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 
 	// Initialize router
 	router.Initialize()
