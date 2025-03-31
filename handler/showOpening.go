bf88c6b4cb986de40cb97c07c691a09d81f9c51d
Matheus Henriques <matheushlg10@gmail.com>
2025-03-31 12:49:03 -0300

    docs: add docummentation for showOpening

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -8,6 +8,19 @@ import (
 	"github.com/hlgboot/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// Show Opening godoc
+// @Summary Show opening
+// @Description Show a job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
